a5f44c3faa8e549e47703caf02dd69943f420b98
LukeGu <luke.gu.work@gmail.com.au>
2022-11-10 20:40:42 +1100

    chore: schema question section

--- schemas/product.js
+++ schemas/product.js
@@ -232,5 +232,29 @@ export default {
         },
       ],
     },
+    {
+      title: 'Question section',
+      name: 'questionSection',
+      type: 'object',
+      fields: [
+        {
+          name: 'questions',
+          type: 'array',
+          of: [
+            {
+              name: 'question',
+              type: 'object',
+              fields: [
+                {
+                  name: 'question',
+                  type: 'string',
+                },
+                { name: 'answer', type: 'text' },
+              ],
+            },
+          ],
+        },
+      ],
+    },
   ],
 };
